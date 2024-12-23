9579c9f8205cde047ed0eb7cfb84ea62b453b40f
Travis Bridle <travis.bridle@gmail.com>
2024-12-23 08:48:45 -0700

    order, order i say

--- docsmith.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Compatibility script - redirects to main.py
-import sys
-from main import main
-import asyncio
-
-if __name__ == "__main__":
-    asyncio.run(main(*sys.argv[1:]))
